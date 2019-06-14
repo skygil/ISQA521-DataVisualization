b73de8ab6f97fe6a0b2bb448e14314a01a8261c6
skygil <46805161+skygil@users.noreply.github.com>
2019-06-13 22:15:04 -0700

    Updated Shiny app and added URL into main RMD file.

--- server.R
+++ server.R
@@ -1,17 +1,94 @@
 #Chess
-library(shiny)
-library(lessR)
-library(ggplot2)
-library(tidyverse)
-library(reshape2)
+#library(shiny)
+#library(lessR)
+#library(ggplot2)
+#library(tidyverse)
+#library(reshape2)
 
 d <- read.csv('data/filtereddata.csv')
 
+#shinyServer(function(input, output) {
+#  output$myPlot <- renderPlot({
+ #                           #' Histogram for # of turns
+#                           Histogram(turns, data=d, bin.width=input$MyWidth,quiet = TRUE, xlab="Number of Turns", ylab="Count of Games")
+#  })
+#})
+#######################################################################################
+library(ggplot2)
+library(shinyBS)
+library("MASS")
+library(ggplot2)
+library(methods)
+library(hexbin)
 
-shinyServer(function(input, output) {
-  output$myPlot <- renderPlot({
-                            #' Histogram for # of turns
-                           Histogram(turns, data=d, bin.width=input$MyWidth,quiet = TRUE, xlab="Number of Turns", ylab="Count of Games")
+d <- read.csv('data/filtereddata.csv')
+
+function(input, output, session){
+  
+#############################
+  #'Histogram
+#############################
+  
+  observeEvent(input$link_to_tabpanel_geom_histogram, {
+    newvalue <- "geom_histogram"
+    updateNavbarPage(session, "main", newvalue)
+  })
+  #############################
+  #' # of turns
+  #############################
+  
+  output$plot_geom_histogram <- renderPlot({
+    ggplot(d, aes(turns)) + geom_histogram(alpha = input$geom_histogram_element1, 
+                                             color = input$geom_histogram_element2, 
+                                             fill = input$geom_histogram_element3,
+                                             linetype = input$geom_histogram_element4,
+                                             size = input$geom_histogram_element5,
+                                             binwidth  = input$geom_histogram_element6) +
+   xlab("Number of Turns") + ylab("Count of Games") + labs( subtitle = "Identify the length of a normal game")    
   })
   
-})
+  output$geom_histogram_text <- renderText({
+    paste('ggplot(d, aes(turns)) + \n geom_histogram(alpha = ',input$geom_histogram_element1,',\n color = "',input$geom_histogram_element2,'",\n fill = "',input$geom_histogram_element3,'",\n linetype = "',input$geom_histogram_element4,'",\n size = ',input$geom_histogram_element5,',\n binwidth = ',input$geom_histogram_element6, ')',sep='' )
+  })
+  
+  output$geom_histogram_table <- renderTable({
+    head(d)
+  })
+  
+  #############################
+  #'Bar Chart  
+  observeEvent(input$link_to_tabpanel_geom_bar, {
+    newvalue <- "geom_bar"
+    updateNavbarPage(session, "main", newvalue)
+  })
+
+  output$plot_geom_bar <- renderPlot({
+    ggplot(d, aes(shortname)) + geom_bar(
+                                              alpha    = input$geom_bar_element1, 
+                                              color    = input$geom_bar_element2, 
+                                              fill     = input$geom_bar_element3,
+                                              linetype = input$geom_bar_element4,
+                                              size     = input$geom_bar_element5,
+                                              width    = input$geom_bar_element6) +  
+      scale_x_discrete(labels = abbreviate) + theme(axis.text.x = element_text(angle=45,  hjust=1)) +
+      #coord_flip() +
+      xlab("Openings") + ylab("Count of Games")
+    
+    
+  })
+  output$geom_bar_text <- renderText({
+    paste('ggplot(d, aes(shortname)) + \n  geom_bar(alpha = ',input$geom_bar_element1,',\n color = "',input$geom_bar_element2,'",\n fill = "',input$geom_bar_element3,'",\n linetype = "',input$geom_bar_element4,'",\n size = ',input$geom_bar_element5,',\n width = ',input$geom_bar_element6, ')',sep='' )
+  })
+  
+  output$geom_bar_table <- renderTable({
+    head(d)
+  })
+  
+  
+  
+}
+
+
+
+
+
--- ui.R
+++ ui.R
@@ -1,23 +1,252 @@
-library(shiny)
-library(lessR)
-library(ggplot2)
-library(tidyverse)
-library(reshape2)
+shinyUI(
+    
+    navbarPage(
+        header = '',
+        id = 'main',
+
+#############################
+ #' Headings/Titles 
+#############################
+
+        title = "The Game of Chess",
+        tabPanel('home', fluidRow(title = '', width = 12,  align = "center", 
+                
+#############################
+ #' Introduction    
+#############################
+
+                tags$body(tags$p(style = "padding: 30px; width:822px",align = 'left',h1("The Game of Chess"), h4("By: Jordan Hilton and Skye Gilbreth"), tags$br(), tags$br(), h2("Welcome!"),
+                        tags$br(),h5("If you are new to the world of chess, or just generally like to nerd out on a statistical analysis from our dataset, then you are in the right place!") ,
+                        tags$br(),'This app was developed to meet the requirements for the final class project in ISQA 521: Analytics Communication and Management. This course took place through the ', tags$a('School of Business,',target="_blank" ,href='https://www.pdx.edu/sba/'), 'at', tags$a('Portland Statue University.',target="_blank" ,href='https://www.pdx.edu/' ),tags$br(),tags$br(),'The main R package we used to write this code, was', tags$a('ggplot2.',target="_blank", href='http://ggplot2.org/'), tags$br(),'The output code listed under each graph, on their own individual pages, will show you the output in ggplot2. This code can be copied and pasted into R. In order for this code to work appropriately, you will need to change your dataset, and your variables. In otherwords, view this code as mostly an example of what to expect when writing something similiar in R and more as code you can take and manipulate.',
+                        tags$br(),'If you are interested in learning more about this project, you can view the data by heading over to this unique ', tags$a('Github',target="_blank" ,href='https://github.com/lykouragh/ISQA521'), ' address.'
+                                           )
+                                    ),
+        
+
+#########################################################################
+ #' Create Table for Pictures/Links to the Histogram and Bar graphs 
+#########################################################################
+
+                tags$table(
+                    tags$tr(tags$td( colspan = 3 ,align = "center" ,tags$table(tags$tr(tags$td(tags$span( style = "font-size:200%", 'Graphs:')))) )),
+                    tags$tr(tags$td( tags$tr(tags$td(actionLink("link_to_tabpanel_geom_histogram",tags$img(src = 'geom_histogram.png'))), tags$td(actionLink("link_to_tabpanel_geom_bar",tags$img(src = 'geom_bar.png')))),
+                                     tags$tr(tags$td(align = "center",tags$span('Histogram')), tags$td(align = "center",tags$span('Bar Chart'))) ))
+                    
+                ) #' End of Tags Table
+            ) #' End of Fluid Row
+        ), #' End of tab panel
+
+##############################
+ #' Navigation to Subpages
+##############################
+        
+#' Navbars    
+        navbarMenu(
+            "Graphs",
+            
+##################################
+ #' Histogram Panel (Design View)
+#################################
+
+            tabPanel(
+                'Histogram',
+                value = 'geom_histogram',   
+                
+                sidebarLayout(
+                    position = "left",
+                    
+                    sidebarPanel(
+                        "Aesthetics",
+                        width = 4,
+                        
+                        sliderInput(
+                            'geom_histogram_element1', 
+                            label='alpha', 
+                            min = 0 , 
+                            max = 1, 
+                            value = 1, 
+                            step = 0.1),
+                        
+                        selectInput(
+                            'geom_histogram_element2', 
+                            label = 'color', 
+                            choices = c('darkblue','grey35','red','#add8e6','green','blue','yellow'),
+                            selected = 'grey35'),
+                        
+                        selectInput(
+                            'geom_histogram_element3', 
+                            label = 'fill', 
+                            choices = c('white','#138D75', 'grey','black','orange','grey35'),
+                            selected = 'grey35'),
+                        
+                        selectInput(
+                            'geom_histogram_element4', 
+                            label = 'linetype', 
+                            choices = c("blank", "solid", "dashed", "dotted", "dotdash", "longdash","twodash"),
+                            selected = 'solid'),
+                        
+                        sliderInput(
+                            'geom_histogram_element5', 
+                            label='size', 
+                            min = 0.1 , 
+                            max = 3, 
+                            value = 0.5, 
+                            step = 0.5),
+                        
+                        sliderInput(
+                            'geom_histogram_element6'
+                            , label='binwidth'
+                            , min = 1 
+                            , max = 100
+                            , value = 15
+                            , step = 0.1),
+                        
+                        checkboxInput(
+                            inputId = "geom_histogram_table",
+                            label  = strong("Show data"),
+                            value  = TRUE),
+                        
+                        checkboxInput(
+                            inputId = "geom_histogram_text",
+                            label   = strong("Show ggplot function"),
+                            value   = FALSE)
+                    ),
+                    
+                    mainPanel(
+                        "Game Length", 
+                        width = 8,
+                        
+                        fluidRow(
+                            style = "padding-right: 10px;",
+                            splitLayout(plotOutput('plot_geom_histogram'))
+                        ),
+                        
+                        conditionalPanel(
+                            condition = "input.geom_histogram_table == true",
+                            
+                            fluidRow(
+                                style = "padding-right: 10px;",
+                                tableOutput("geom_histogram_table")
+                            )
+                        ),
+                        
+                        conditionalPanel(
+                            condition = "input.geom_histogram_text == true",
+                            fluidRow(
+                                style = "padding-right: 20px;",
+                                textOutput("geom_histogram_text", container = pre)
+                            )
+                        )
+                    ) #' End of main panel
+                ) #' End of sidebar layout
+            ), #' End of tab panel
+            
+#############################
+ #' Bar Chart Panel
+#############################
+
+            tabPanel(
+                'Bar Chart',
+                value = 'geom_bar',   
+                
+                sidebarLayout(
+                    position = "left",
+                    
+                    sidebarPanel(
+                        "Aesthetics",
+                        width = 4,
+                        
+                        sliderInput(
+                            'geom_bar_element1', 
+                            label='alpha', 
+                            min = 0 , 
+                            max = 1, 
+                            value = 1, 
+                            step = 0.1),
+                        
+                        selectInput(
+                            'geom_bar_element2', 
+                            label = 'color', 
+                            choices = c('darkblue','grey35','red','#add8e6','green','blue','yellow'),
+                            selected = 'grey35'),
+                        
+                        selectInput(
+                            'geom_bar_element3', 
+                            label = 'fill', 
+                            choices = c('white','#138D75', 'grey','black','orange','grey35'),
+                            selected = 'grey35'),
+                        
+                        selectInput(
+                            'geom_bar_element4', 
+                            label = 'linetype', 
+                            choices = c("blank", "solid", "dashed", "dotted", "dotdash", "longdash","twodash"),
+                            selected = 'solid'),
+                        
+                        sliderInput(
+                            'geom_bar_element5', 
+                            label='size', 
+                            min = 0.1 , 
+                            max = 3, 
+                            value = 0.5, 
+                            step = 0.5),
+                        
+                        sliderInput(
+                            'geom_bar_element6'
+                            , label='binwidth'
+                            , min = 1 
+                            , max = 25
+                            , value = 1.5
+                            , step = 0.1),
+                        
+                        checkboxInput(
+                            inputId = "geom_bar_table",
+                            label  = strong("Show data"),
+                            value  = TRUE),
+                        
+                        checkboxInput(
+                            inputId = "geom_bar_text",
+                            label   = strong("Show ggplot function"),
+                            value   = FALSE)
+                    ),
+                    
+                    mainPanel(
+                        "Frequency of Openings", 
+                        width = 8,
+                        
+                        fluidRow(
+                            style = "padding-right: 10px;",
+                            splitLayout(plotOutput('plot_geom_bar'))
+                        ),
+                        
+                        conditionalPanel(
+                            condition = "input.geom_bar_table == true",
+                            
+                            fluidRow(
+                                style = "padding-right: 10px;",
+                                tableOutput("geom_bar_table")
+                            )
+                        ),
+                        
+                        conditionalPanel(
+                            condition = "input.geom_bar_text == true",
+                            fluidRow(
+                                style = "padding-right: 20px;",
+                                textOutput("geom_bar_text", container = pre)
+                            )
+                        )
+                    ) #' End of main panel
+                ) #' End of sidebar layout
+            ) 
+
+#############################
+ #' End of tab panel
+##############################
+            
+        ) #' End of NavBar Menu
+    ) #' End of Page
+ )#' End of App
 
 
-shinyUI(fluidPage(
-    titlePanel("Select Bin Width"),
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput(inputId = "MyWidth", # Input: Slider for the number of bins
-                        label = "Bin Width:",
-                        min = 1, max = 100, value = 15)
-        ),
-        mainPanel(
-            plotOutput(outputId = "myPlot") # Output: Histogram
-        )
-    )
-))
 
 
 
